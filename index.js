3b69beff3b203a2ad130dbd05079fea420039c4c
Asif Vora <asifvora0@gmail.com>
2021-08-12 10:07:04 +0530

    Added V8 engine flow gif file

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 
 
 
-// function a(){
-//     var v = 56
-//     function b() {
-//         var a = 7;
-//         function c() {
-//           var b = 76;
-//           function d() {
-//             console.log(a, b,v);
-//           }
-//           d();
-//         }
-//         c();
-//       }
-//       b()
-// }
-// a();
+function a(){
+    var v = 56
+    function b() {
+        var a = 7;
+        function c() {
+          var b = 76;
+          function d() {
+            console.log(a, b,v);
+          }
+          d();
+        }
+        c();
+      }
+      b()
+}
+a();
 
 // function x(){
 //     // for(let i =1; i<=5; i++){
